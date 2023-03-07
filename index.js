76649ff2b847a4fecd96c99690c2a225a0bc0315
gghast <92589763+gghast@users.noreply.github.com>
2023-03-07 21:36:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require("discord.js");
 
 const client = new Client({intents: new Intents(32767)});
 
-client.login("MTA4MTYwMTU2NzI5NzM5MjY4Mw.GdKQjA.JH6prpIBOhvcBBNeVJNnsWlBy4jdfDCPmErLU8");
+client.login("MTA4MTYwMTU2NzI5NzM5MjY4Mw.GRm5Wo.E_rbLapnsKV0IL1LCnr_X4wlk5Y6R11QIUV7Hs");
 
 client.on("ready", () => {
     client.user.setActivity(
